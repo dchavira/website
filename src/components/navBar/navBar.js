bc0dedc58431258f6ede87a320d99c8a1bcc11fb
dchavira <37552297+dchavira@users.noreply.github.com>
2019-12-18 03:02:45 -0700

    NavBar scroll
    Made navbar change colors when scrolled down so client can read it

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import './navBar.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FaAlignJustify } from 'react-icons/fa';
 class NavigationBar extends React.Component {
     state = {
-        toggle: false
+        toggle: false,
+        scrolled: false
+    }
+    constructor(props){
+        super(props);
+        this.handleScroll= this.handleScroll.bind(this);
     }
     Toggle = () => {
         this.setState({ toggle: !this.state.toggle })
     }
+    toggleScroll =() =>{
+        this.setState({ scrolled: !this.state.scrolled});
+        
+    }
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll, { passive: true })
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = event => {
+        // do something at end of scroll
+        let element=event.target;
+        
+        if(!this.state.scrolled){
+            this.toggleScroll()
+        }if(window.pageYOffset===0 && this.state.scrolled){
+            this.toggleScroll()
+            console.log(window.pageYOffset)
+            
+            
+        }
+    }
+
     render() {
         const li = [
             {
@@ -29,11 +60,14 @@ class NavigationBar extends React.Component {
                 text: "Contact"
             }
         ]
+
         const currentLoc = window.location.pathname;
-        console.log(currentLoc);
+
+
+
         return (
-            
-            <nav>
+
+            <nav  onScroll={this.handleScroll} className={this.state.scrolled ? "scrolled":null}>
                 {//<li className={text === 'foo' ? styles.class1 : styles.class2 } .../>
                 }<Link to="/home"><img className="logo" src="./img/Logo Transparent.png" alt="Logo" ></img></Link>
                 <button onClick={this.Toggle}>
@@ -42,14 +76,15 @@ class NavigationBar extends React.Component {
                 <ul className={this.state.toggle ? "nav-links show-nav" : "nav-links"}>
                     {
                         li.map((objLink, i) => {
-                            return (<li key={i} ><Link to={objLink.link}>{objLink.text}</Link></li>)
+                            return (<li key={i} ><Link to={objLink.link} style={{ color: currentLoc === "/" ? "white" : "black" }}>{objLink.text}</Link></li>)
                         })
                     }
 
                 </ul>
-                
+
+
             </nav>
-            );
+        );
 
 
     }
